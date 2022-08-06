958705fcce3d1693e489d4a5f04f540f1a948578
kmatulud <princessmakgotso@gmail.com>
2022-08-06 15:29:02 +0200

    initial

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.send("backend deployed");
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
     console.log(`App running on port ${PORT}`);
