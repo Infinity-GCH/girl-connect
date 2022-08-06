712ace657bb215dd8d61c976120614c10fcc42da
Florah <fweni@student.wethinkcode.co.za>
2022-08-06 16:31:12 +0200

    registration method added

--- routes/routes.js
+++ routes/routes.js
@@ -18,3 +18,23 @@ if (process.env.NODE_ENV == "production") {
 }
 
 const db = pgp(config);
+
+// Registration
+router.post("/register", async (req, res) => {
+    try {
+        const {full_name, email, password} = req.body;
+        bcrypt.hash(password, 10).then(async function (hash){
+            let userDetails = `insert into users (f_name, e_mail, p_word) values ($1,$2,$3)`;
+            await db.none(userDetails, [full_name, email, hash]);
+        });
+        res.json({
+            status: "success",
+        });
+    } catch (e) {
+        console.log(e);error
+        res.json({
+            status: "error",
+            error: e.message,
+        })
+    }
+})
