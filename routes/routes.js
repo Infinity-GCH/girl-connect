1901b3aaaec4645ab1d3ed6ebba7c548397e1cd2
Kgotso Matuludi <50702696+Kmatulud@users.noreply.github.com>
2022-08-07 04:57:03 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -41,9 +41,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
 	try {
 		const { password, email } = req.body;
-		const user = await db.oneOrNone(`select * from users where email=$1`, [
-			email,
-		]);
+		const user = await db.oneOrNone(`select * from users where email=$1`, [email]);
 		console.log(user);
 		const dbPassword = user.password;
 		const validPass = await bcrypt.compare(password, dbPassword);
@@ -63,9 +61,7 @@ router.post("/login", async (req, res) => {
 router.post("/request", async (req, res) => {
 	try {
 		const { type, description, email } = req.body;
-		let userRef = await db.one("select user_id from users where email = $1", [
-			email,
-		]);
+		let userRef = await db.one("select user_id from users where email = $1", [email]);
 
 		let userId = userRef.user_id;
 		await db.none(`insert into requests (type, description, status, user_id) values ($1,$2,$3, $4)`, [type, description, "open", userId]);
